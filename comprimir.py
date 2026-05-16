aa4289438d7f0ef2ad397c88d03edd9fc191c163
Josué Calderón <josuecalderonbusiness@gmail.com>
2026-05-16 14:46:34 -0500

    imagenes quimica dia6

--- comprimir.py
+++ comprimir.py
@@ -1,7 +1,7 @@
 from PIL import Image
 import os
 
-carpeta = r'C:\guia_sexto\public\imagenes\qui\dia4'
+carpeta = r'C:\guia_sexto\public\imagenes\qui\dia6'
 
 for nombre in os.listdir(carpeta):
     ext = os.path.splitext(nombre)[1].lower()
