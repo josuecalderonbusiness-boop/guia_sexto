fa658837adb081adb925318203405f317cccdd41
Josué Calderón <josuecalderonbusiness@gmail.com>
2026-05-15 19:09:18 -0500

    imagenes lenguaje dia5

--- comprimir.py
+++ comprimir.py
@@ -1,7 +1,7 @@
 from PIL import Image
 import os
 
-carpeta = r'C:\guia_sexto\public\imagenes\len\dia4'
+carpeta = r'C:\guia_sexto\public\imagenes\len\dia5'
 
 for nombre in os.listdir(carpeta):
     if nombre.lower().endswith('.png'):
